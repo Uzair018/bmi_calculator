7ff2a126fc9319a9a06760caf6776428d2de47a3
Uzair018 <100482577+Uzair018@users.noreply.github.com>
2022-04-14 15:56:11 +0500

    use of gesture

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconTextFile.dart';
 import 'ContainerFile.dart';
+
+const activeColour = Color(0xFF1D1E33);
+const deActiveColour = Color(0xFF111328);
 
 
 
@@ -12,27 +15,50 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deActiveColour;
+  Color feMaleColor = deActiveColour;
+
+  void updateColour(int gender) {
+    if (gender == 1) {
+      maleColor = activeColour;
+      feMaleColor = deActiveColour;
+    }
+    if (gender == 2) {
+      maleColor = activeColour;
+      feMaleColor = deActiveColour;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var malecolor;
+    var feMaleColor;
     return Scaffold(
         appBar: AppBar(
           title: Text('BMI CALCULATOR'),
         ),
-        body:Column(
+        body: Column(
           children: [
             Expanded(child: Row(
               children: [
                 Expanded(
-                  child: RepeatContainerCode(
-                    colors: Color(0xFF1D1E33),
-                    cardWidget: RepeatTextandIconWidget(
-                      iconData: FontAwesomeIcons.male,
-                      label: 'Male',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(2);
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colors: maleColor,
+                      cardWidget: RepeatTextandIconWidget(
+                        iconData: FontAwesomeIcons.male,
+                        label: 'Male',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(child: RepeatContainerCode(
-                  colors: Color(0xFF1D1E33),
+                  colors: feMaleColor,
                   cardWidget: RepeatTextandIconWidget(
                     iconData: FontAwesomeIcons.female,
                     label: 'Female',
@@ -40,7 +66,7 @@ class _InputPageState extends State<InputPage> {
                 ),),
               ],
             ),),
-            Expanded(child:RepeatContainerCode(
+            Expanded(child: RepeatContainerCode(
               colors: Color(0xFF1D1E33),
             ),),
             Expanded(child: Row(
@@ -58,6 +84,7 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
 
 
 
