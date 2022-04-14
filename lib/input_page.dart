e536cbbed7f48a92993086ebe48542de5a9f1506
Uzair018 <100482577+Uzair018@users.noreply.github.com>
2022-04-14 10:32:05 +0500

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body:Column(
+          children: [
+            Expanded(child: Row(
+              children: [
+                Expanded(
+                  child: RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
+                    cardWidget: RepeatTextandIconWidget(),
+                  ),
+                ),
+                Expanded(child: RepeatContainerCode(
+                  colors: Color(0xFF1D1E33),
+                  cardWidget: RepeatTextandIconWidget(),
+                ),),
+              ],
+            ),),
+            Expanded(child:RepeatContainerCode(
+              colors: Color(0xFF1D1E33),
+            ),),
+            Expanded(child: Row(
+              children: [
+                Expanded(child: RepeatContainerCode(
+                  colors: Color(0xFF1D1E33),
+                ),),
+                Expanded(child: RepeatContainerCode(
+                  colors: Color(0xFF1D1E33),
+                ),),
+              ],
+            ),),
+          ],
+        )
+    );
+  }
+}
+
+class RepeatTextandIconWidget extends StatelessWidget {
+  final IconData iconData;
+  final String label;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+     children: [
+       Icon(
+         iconData,
+         size: 85.0,
+       ),
+       SizedBox(
+         height: 15.0,
+       ),
+       Text('Male',style: TextStyle(
+         fontSize: 30.0,
+       ),),
+     ],
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({@required this.colors,this.cardWidget});
+final Color colors;
+final Widget cardWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: cardWidget,
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
